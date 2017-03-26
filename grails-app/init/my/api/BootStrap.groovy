332a66bebc9ab8030d1559b87c8dc89f7b518c5d
Jonas Witt <jonas.witt@valsight.com>
2017-03-26 12:18:09 +0200

    Create test entity on bookstrap

--- grails-app/init/my/api/BootStrap.groovy
+++ grails-app/init/my/api/BootStrap.groovy
@@ -1,8 +1,11 @@
 package my.api
 
+import org.bookstore.Book
+
 class BootStrap {
 
     def init = { servletContext ->
+        new Book(title: 'Book 1', author: 'Book 1').save(failOnError: true)
     }
     def destroy = {
     }
